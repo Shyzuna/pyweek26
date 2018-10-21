ee5faabfa0e665bf35050a62bf1b5b2fe53b1d55
StripedBanana <jnsgs2@gmail.com>
2018-10-21 19:08:13 +0200

    Ajout premiers objets

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pygame
+from objects.SolarPanel import SolarPanel
+from objects.Battery import Battery
 from pygame.locals import *
+
 
 if __name__ == '__main__':
     pygame.init()
+
+    sp = SolarPanel([1, 1])
+    ba = Battery([1, 1])
+
+    while(1):
+        sp.update()
+        ba.update_capacity(11)
+        print("fin")
---
+++ objects/SolarPanel.py
@@ -0,0 +1,41 @@
+from objects.Building import Building
+
+
+class SolarPanel(Building):
+
+    def __init__(self, position):
+
+        self.position = position
+        self.size = [1, 1]
+        self.connections = {'inputs': None,
+                            'outputs': {'Electricity': False}}
+        self.max_prod = 2
+        self.cur_prod = 0
+
+        Building.__init__(self, self.position, self.size, self.connections)
+
+    def update(self):
+        # Update production
+        if game_manager.is_night:
+            self.cur_prod = 0
+            # do nothing
+        else:
+            self.cur_prod = self.max_prod
+            to_deliver = self.cur_prod
+            # Giving away power to non full batteries in same network
+            for battery in game_manager.get_batteries():
+                cur_capacity = battery.cur_capacity
+                max_capacity = battery.max_capacity
+                if battery.is_full():
+                    # go to next battery
+                    pass
+                elif cur_capacity + to_deliver <= max_capacity:
+                    # this one can take everything
+                    battery.update_capacity(to_deliver)
+                    to_deliver = 0
+                else:
+                    # partial delivery
+                    battery.fill()
+                    to_deliver = to_deliver - (max_capacity - cur_capacity)
+
+        # TODO Update connections array
